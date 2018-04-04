3bbf4fadf1e56d1e5915e313ec264e9985bc399a
Cimeriss <bmaiiwenn@gmail.com>
2018-04-04 10:29:20 +0200

    new class : reactor

---
+++ src/worker/reactor.js
@@ -0,0 +1,11 @@
+class Reactor {
+	constructor(physicsObject, position, thrust, activationTime, extinctionTime, angle);
+	this.physicsObject = physicsObject;
+	this.position = position;
+	this.thrust = thrust;
+	this.activationTime = activationTime;
+	this.extinctionTime = extinctionTime;
+	this.angle = angle;
+
+	draw() {}
+}
