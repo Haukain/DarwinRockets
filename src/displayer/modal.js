a0638c6b531d35c01680ed6ffc6ae89176c163ff
titaniumbaryum <titouan.baillon@gmail.com>
2018-04-04 16:09:55 +0200

    [displayer] modal

--- src/displayer/modal.js
+++ src/displayer/modal.js
@@ -6,6 +6,19 @@ export class Modal extends Row{
     this.element.className = "modal";
     let uid = ""+Math.round(Math.random()*100000);
     this.element.id= `modal-${uid}`;
+    //Modal-dialog
+    this._dialog = document.createElement("DIV");
+    this._dialog.className = "modal-dialog";
+    this.element.appendChild(this._dialog);
+    //Modal-content
+    this._content = document.createElement("DIV");
+    this._content.className = "modal-content";
+    this._dialog.appendChild(this._content);
+    //Modal-body
+    this.innerElement = document.createElement("DIV");
+    this.innerElement.className = "modal-body";
+    this._content.appendChild(this.innerElement);
+
     $(this.element).modal();
     $(this.element).modal('hide');
   }
