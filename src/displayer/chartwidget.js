ef4aa15d56b4a92e8150fd78d7b72d00579fad05
titaniumbaryum <titouan.baillon@gmail.com>
2018-04-04 17:25:43 +0200

    [displayer] chartwidget

--- src/displayer/chartwidget.js
+++ src/displayer/chartwidget.js
@@ -1,16 +1,9 @@
 import { CanvasWidget } from "./canvaswidget.js";
 
 export class ChartWidget extends CanvasWidget{
-  constructor(bg,fg,type,data) {
+  constructor(bg,fg,options) {
     super(bg,fg);
-    this.type = type;
-    this.data = data;
     this.ctx = this.canvas.getContext('2d');
-    this.chart = new Chart(this.ctx, {type : this.type, data : this.data});
+    this.chart = new Chart(this.ctx, options);
   }
-  update(){
-  	this.chart.update();
-  }
-
 }
-
