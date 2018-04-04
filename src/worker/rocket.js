39e97ea26877be624462f8b3e11e43611809e2fa
Cimeriss <bmaiiwenn@gmail.com>
2018-04-04 10:43:05 +0200

    rocket : get/set

--- src/worker/rocket.js
+++ src/worker/rocket.js
@@ -1,9 +1,9 @@
 class Rocket {
 	constructor(physicsObject, dead);
-	this.physicsObject = physicsObject;
-	this.dead = dead;
-	this.parents = [];
-	this.reactors = [];
+	this._physicsObject = physicsObject;
+	this._dead = dead;
+	this._parents = [];
+	this._reactors = [];
 
 	draw() {}
 }
@@ -15,3 +15,15 @@ addParents(parent) {
 addReactors(reactor) {
 	this.reactors.push(reactor);
 }
+
+getPhysicsObject() {return this._physicsObject;}
+setPhysicsObject(physicsObject) {this._physicsObject = physicsObject;}
+
+getStatus() {return this._dead;}
+setStatus(status) {this._dead = status;}
+
+getParents() {return this._parents;}
+setParents(parents) {this._parents = parents;}
+
+getReactors() {return this._reactors;}
+setReactors(reactors) {this._reactors = reactors;}
