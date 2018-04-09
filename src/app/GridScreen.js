7c3baf0ea2bc625d4872905ee1f1b69067d6ba18
l5chaill <37894493+l5chaill@users.noreply.github.com>
2018-04-09 10:49:18 +0200

    RocketViewer.js / GridScreen.js

---
+++ src/app/GridScreen.js
@@ -0,0 +1,13 @@
+import { Screen } from "../Screen.js";
+
+export class GridScreen extends Screen{
+
+	constructor(app, gen) {
+        super(app);
+        this._gen = gen;
+    }
+
+    this.display(){
+    }
+
+}
