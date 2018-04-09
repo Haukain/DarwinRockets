54a66fc05cd7cb575a1773da4f6c2c23f6388c9c
adrienConstante <37894502+adrienConstante@users.noreply.github.com>
2018-04-09 10:30:04 +0200

    Button StartScreen

--- src/app/StartScreen.js
+++ src/app/StartScreen.js
@@ -1,8 +1,23 @@
+import { Button } from "../displayer/Button/Button.js";
+
 export class StartScreen{
 	constructor(app){
+		let that = this;
+		this._app=app;
+		this._EditButton = new Button
+		//back button
+    	this._addButton("arrow_back","go back",e=>{that._app.goBack()});
+    	//start/stop gen Button
+    	this._addButton("play_arrow","toggle continuous generation",e=>{
+      		if(e.icon=="play_arrow"){
+        		e.icon = "pause";
+        		that._app.startGen();
+      		}else{
+        		e.icon = "play_arrow";
+        		that._app.stopGen();
+      		}
 
 
 
 
-	
 }
