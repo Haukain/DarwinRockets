c901ced4b859b6c3a02b76134ee22225ffdddfb9
l5chaill <37894493+l5chaill@users.noreply.github.com>
2018-04-10 15:43:25 +0200

    testScreens validé par titouan oklm

--- src/testScreens.js
+++ src/testScreens.js
@@ -1,7 +1,13 @@
 import { Screen } from "./app/Screen.js";
 import { App } from "./app/App.js";
+import { GridScreen } from "./app/GridScreen.js";
+import { Generation } from "./worker/Generation.js";
 
 let app = new App(document.body);
 let screen = new Screen(app);
 screen.destroy();
 console.log(screen.container);
+
+let gen = new Generation();
+gen.addRockets(1)
+let gridscreen = new GridScreen(app,gen);
