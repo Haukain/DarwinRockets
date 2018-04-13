5ab5b7510f3f25e9f2cd3ec59f26ddde718866d3
titaniumbaryum <titouan.baillon@gmail.com>
2018-04-13 12:27:05 +0200

    edit screen slighly better layout

--- src/app/EditScreen.js
+++ src/app/EditScreen.js
@@ -1,6 +1,5 @@
 import { Screen } from "./Screen.js";
 import { Col } from "../displayer/Col.js";
-import { CenteredRow } from "../displayer/CenteredRow.js";
 import { Card } from "../displayer/Card.js";
 import { Title } from "../displayer/Title.js";
 import { RangeInput } from "../displayer/RangeInput.js";
@@ -11,18 +10,20 @@ export class EditScreen extends Screen{
 	constructor(app) {
 		super(app);
 		//structure
-		let leftCol=new Col(12,12,6,6);
-		let rightCol=new Col(12,12,6,6);
+		let leftCol=new Col(12,12,5,5);
+		let rightCol=new Col(12,12,7,7);
 		this._container.addChild(leftCol);
 		this._container.addChild(rightCol);
 		//config
-		let crl = new CenteredRow();
-		leftCol.addChild(crl);
 		let confCard = new Card("white","navyblue");
-		crl.addChild(confCard);
+		leftCol.addChild(confCard);
 		let confCardTitle = new Title("Configuration");
 		confCard.addChild(confCardTitle);
 		let testRange = new RangeInput();
+		testRange.min=-1;
+		testRange.max=1;
+		testRange.step=.01;
+		testRange.text="test range";
 		confCard.addChild(testRange);
 		//terrain
 		this._terrainConf = new TerrainConfigurator();
