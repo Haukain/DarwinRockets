af62ed547bf77cd4066e0c009e35025e3831d75f
Cimeriss <bmaiiwenn@gmail.com>
2018-04-18 10:32:48 +0200

    Draw start

--- src/worker/Start.js
+++ src/worker/Start.js
@@ -1,6 +1,13 @@
-class Start extends TerrainObject {
-	constructor(attraction, position);
-	super(attraction, position);
+export class Start extends TerrainObject {
+	constructor(attraction, position) {
+		super(attraction, position);
+		this._radius = 10;
+	}
 
-	draw() {}
+	draw(attraction, position, radius, context) {
+			context.translate(position.x,position.y)
+			context.beginPath();
+			context.arc(0,0,radius,0,2*Math.PI);
+			context.fill();
+	}
 }
