339d02c829c090036540f6450f2d84ee850afdce
Cimeriss <bmaiiwenn@gmail.com>
2018-04-18 10:35:02 +0200

    Draw End

--- src/worker/End.js
+++ src/worker/End.js
@@ -1,6 +1,13 @@
-class End extends TerrainObject {
-	constructor(attraction, position);
-	super(attraction, position);
+export class End extends TerrainObject {
+	constructor(attraction, position) {
+		super(attraction, position);
+		this._radius = 10;
+	}
 
-	draw() {}
+	draw(attraction, position, radius, context) {
+		context.translate(position.x,position.y)
+		context.beginPath();
+		context.arc(0,0,radius,0,2*Math.PI);
+		context.fill();
+	}
 }
