8204c67cdff4a0eb4601068512ca981862fb482b
Cimeriss <bmaiiwenn@gmail.com>
2018-04-23 10:19:39 +0200

    Affichage dans Planet

--- src/worker/PhysicsPlanet.js
+++ src/worker/PhysicsPlanet.js
@@ -4,21 +4,9 @@ export class PhysicsPlanet extends PhysicsObstacle{
 	constructor(position,radius){
 		super(position,radius);
 	}
-	createObject(position,radius){
-		let fs = '#bae1ff';
-		if(Math.random()>0.5){
-			fs = '#baffc9';
-		}
-
-		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true,render:{fillStyle:fs}})
+	
+		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true})
 		Matter.Body.setDensity(this._object,0.005);
 		this._object.label = "planet";
-	}
-
-	draw(ctx){
-		ctx.beginPath();
-		ctx.fillStyle = this._object.render.fillStyle;
-		ctx.arc(0,0,this._radius,0,2*Math.PI);
-		ctx.fill();
 	}
 }
