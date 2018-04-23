2ced1cbb4d7778999431b7d684f730a0f8c45be6
Cimeriss <bmaiiwenn@gmail.com>
2018-04-23 10:22:32 +0200

    Bêtise réparée

--- src/worker/PhysicsPlanet.js
+++ src/worker/PhysicsPlanet.js
@@ -4,7 +4,8 @@ export class PhysicsPlanet extends PhysicsObstacle{
 	constructor(position,radius){
 		super(position,radius);
 	}
-	
+
+	createObject(position,radius) {
 		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true})
 		Matter.Body.setDensity(this._object,0.005);
 		this._object.label = "planet";
