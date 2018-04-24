156daefd029d8634ec032300bed6f57cd29a82dd
Cimeriss <bmaiiwenn@gmail.com>
2018-04-24 14:35:27 +0200

    C'est fini les bêtises, maintenant je teste

--- src/worker/PhysicsPlanet.js
+++ src/worker/PhysicsPlanet.js
@@ -9,7 +9,8 @@ export class PhysicsPlanet extends PhysicsObstacle{
 		let fs = '#bae1ff';
 		if(Math.random()>0.5){
 			fs = '#baffc9';
-		
+		}
+
 		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true,render:{fillStyle:fs}})
 
 		Matter.Body.setDensity(this._object,0.005);
