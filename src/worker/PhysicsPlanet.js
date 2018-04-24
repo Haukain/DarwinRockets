da81f678037a613e92f162055db1cd282a7ac7f2
Cimeriss <bmaiiwenn@gmail.com>
2018-04-24 14:31:01 +0200

    Retour PhysicsPLanet initial

--- src/worker/PhysicsPlanet.js
+++ src/worker/PhysicsPlanet.js
@@ -4,23 +4,14 @@ export class PhysicsPlanet extends PhysicsObstacle{
 	constructor(position,radius){
 		super(position,radius);
 	}
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-	createObject(position,radius) {
-=======
-	
->>>>>>> parent of 2ced1cb... Bêtise réparée
-		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true})
-=======
 	createObject(position,radius){
 		let fs = '#bae1ff';
 		if(Math.random()>0.5){
 			fs = '#baffc9';
-		}
-
+		
 		this._object = Matter.Bodies.circle(position.x,position.y,radius,{isStatic : true,render:{fillStyle:fs}})
->>>>>>> parent of 8204c67... Affichage dans Planet
+
 		Matter.Body.setDensity(this._object,0.005);
 		this._object.label = "planet";
 	}
