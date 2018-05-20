a8152f4cf5c5fbc711b298df4b9e023abc288b64
Titouan Baillon <titouan.baillon@gmail.com>
2018-05-20 18:09:54 +0200

    modified header buttons

--- src/app/App.js
+++ src/app/App.js
@@ -63,12 +63,15 @@ export class App{
   goStart(){
     this._currentScreen = new StartScreen(this);
     this._state = "start";
+    this._configuration = new Configuration();
     this._header.hideButtons();
     history.pushState({fake: true},"Darwin Rockets","start.html");
   }
   goEdit(locked=false) {
     this._currentScreen = new EditScreen(this,locked);
     this._state = "edit";
+    this._currentGeneration = null;
+    this._generations=[];
     this._header.hideButtons();
     this.stopGen();
     history.pushState({fake: true},"Edit config","edit.html");
