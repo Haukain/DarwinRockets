b036264f530354be127c5045bd869c6fc7d33578
Haukain <Haukain@users.noreply.github.com>
2018-05-23 11:34:05 +0200

    0 to 1 parameters edit screen

--- src/app/EditScreen.js
+++ src/app/EditScreen.js
@@ -42,10 +42,10 @@ export class EditScreen extends Screen{
 				title:"Score calculation",
 				help:"Choose the impact of each parameter on the score calculation\n\n" + "Distance to objective : Less remaining distance gives a better score \n" + "Time of flight : Shorter time of completion gives a better score\n" + "Complexity : A higher percentage of useful reactors gives a better score",
 				params:[
-					{key:".fitnessFunction.remainingDistanceFactor",name:"Distance to objective",min:"-1",max:"1",step:".01"},
-					{key:".fitnessFunction.completionTimeFactor",name:"Time of flight",min:"-1",max:"1",step:".01"},
-					{key:".fitnessFunction.traveledDistanceFactor",name:"Distance traveled",min:"-1",max:"1",step:".01"},
-					{key:".fitnessFunction.complexityFactor",name:"Rocket complexity",min:"-1",max:"1",step:".01"}
+					{key:".fitnessFunction.remainingDistanceFactor",name:"Distance to objective",min:"0",max:"1",step:".01"},
+					{key:".fitnessFunction.completionTimeFactor",name:"Time of flight",min:"0",max:"1",step:".01"},
+					{key:".fitnessFunction.traveledDistanceFactor",name:"Distance traveled",min:"0",max:"1",step:".01"},
+					{key:".fitnessFunction.complexityFactor",name:"Rocket complexity",min:"0",max:"1",step:".01"}
 				]
 			},
 			{
