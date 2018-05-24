5e6c1a4f8f94655f9e856b832c4dabe7969a0a01
Titouan Baillon <titouan.baillon@gmail.com>
2018-05-24 11:25:19 +0200

    col xs fix

--- src/displayer/Col.js
+++ src/displayer/Col.js
@@ -3,6 +3,6 @@ import { Widget } from "./Widget.js";
 export class Col extends Widget{
   constructor(sm,md,lg,xl) {
     super();
-    this._element.className = `colCont col-sm-${sm} col-md-${md} col-lg-${lg} col-xl-${xl}`;
+    this._element.className = `colCont col-${sm} col-sm-${sm} col-md-${md} col-lg-${lg} col-xl-${xl}`;
   }
 }
