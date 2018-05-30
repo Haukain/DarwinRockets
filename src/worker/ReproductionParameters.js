b26ce4cadd5631f452f6fe42720003dd9af4b993
l5chaill <37894493+l5chaill@users.noreply.github.com>
2018-05-30 11:04:20 +0200

    suppresstion randomMutationRate

--- src/worker/ReproductionParameters.js
+++ src/worker/ReproductionParameters.js
@@ -1,26 +1,23 @@
 export class ReproductionParameters {
 	static fromStructure(s){
-		return new ReproductionParameters(s.newGeneAppearanceRate,s.geneDistributionDeviationFactor,s.randomMutationRate);
+		return new ReproductionParameters(s.newGeneAppearanceRate,s.geneDistributionDeviationFactor);
 	}
-	constructor(ngar, gdd, rmr){
+	constructor(ngar, gdd){
 		this._newGeneAppearanceRate = ngar||0;
 		this._geneDistributionDeviationFactor = gdd||0;
-		this._randomMutationRate = rmr||0;
 	}
+
 	get newGeneAppearanceRate() {return this._newGeneAppearanceRate;}
 	set newGeneAppearanceRate(ngar) {this._newGeneAppearanceRate = ngar;}
 
 	get geneDistributionDeviationFactor() {return this._geneDistributionDeviationFactor;}
 	set geneDistributionDeviationFactor(gdd) {this._geneDistributionDeviationFactor = gdd;}
 
-	get randomMutationRate() {return this._randomMutationRate;}
-	set randomMutationRate(rmr) {this._randomMutationRate = rmr;}
 
 	toStructure(){
 		return {
 			newGeneAppearanceRate:this._newGeneAppearanceRate,
 			geneDistributionDeviationFactor:this._geneDistributionDeviationFactor,
-			randomMutationRate:this._randomMutationRate,
 		};
 	}
 }
